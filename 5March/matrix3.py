29a4abd0017f9b6962e59da161e4981247fd77d8
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-05 12:24:20 +0530

    Create matrix3.py

---
+++ 5March/matrix3.py
@@ -0,0 +1,12 @@
+r=int(input('enter rows: '))
+c=int(input('enter columns: '))
+for i in range(1,r+1):
+    for j in range(1,c+1):
+        if i+j<r+1 :
+            print('* ',end='')
+        elif i+j>r+1:
+            print('# ',end='')
+        else:
+            print('@ ',end='')
+    print()
+
