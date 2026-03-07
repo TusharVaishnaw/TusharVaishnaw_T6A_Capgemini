7156c983d1c636f9fd8c421c8c9df2d77107e9b7
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-07 13:27:10 +0530

    Add strong_passwords method to filter passwords
    Implement a method to filter strong passwords based on criteria.

---
+++ Assesments/March7th/9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strong_passwords(self, passwords):
+        strong = []
+        
+        for pwd in passwords:
+            if (len(pwd) >= 8 and
+                any(c.isupper() for c in pwd) and
+                any(c.isdigit() for c in pwd) and
+                any(c in "@#$" for c in pwd)):
+                
+                strong.append(pwd)
+        
+        return strong
