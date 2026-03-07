a7cafa4f4f067a3d3974168e2b9399d7fbe2c506
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-07 10:58:08 +0530

    Update 1.py

--- Classrooms_Practice/7March/1.py
+++ Classrooms_Practice/7March/1.py
@@ -11,9 +11,19 @@ def form(**a):
     print('a:',a)
     print(len(a))
 form(a='Tushar',b='vaishnawtushar@gmail.com',c=9509494140)
-'''
 
 def form(*a):
     print('a:',a)
     print(len(a))
 form('Tushar','vaishnawtushar@gmail.com',9509494140)
+'''
+#Using all four types
+#Positional, keyword, default and variable(dict/tuple) argument
+def form(name,mail,*ph,age=20,alt_ph=None,**l1):
+    print(name)
+    print(mail)
+    print(ph)
+    print(age)
+    print(alt_ph)
+    print(l1)
+form('Tushar','vaishnawtushar@gmail.com',9509494140,2123,123123,alt_ph=89494949410,a=1212,b=2323,c=23)
