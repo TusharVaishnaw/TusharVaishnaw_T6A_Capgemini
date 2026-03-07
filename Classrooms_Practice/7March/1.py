d8061e1a1eb0d39127da5fee24ea96efa0143caa
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-07 10:53:04 +0530

    Update 1.py

--- Classrooms_Practice/7March/1.py
+++ Classrooms_Practice/7March/1.py
@@ -6,8 +6,14 @@ def form(name,mail,ph,age=20,alt_ph=None):
     print(age)
     print(alt_ph)
 form(mail='Tushar',name='vaishnawtushar@gmail.com',9509494140,alt_ph=89494949410)
-'''
+
 def form(**a):
     print('a:',a)
     print(len(a))
 form(a='Tushar',b='vaishnawtushar@gmail.com',c=9509494140)
+'''
+
+def form(*a):
+    print('a:',a)
+    print(len(a))
+form('Tushar','vaishnawtushar@gmail.com',9509494140)
