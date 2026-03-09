867e8a41268c93a4bed02cfff450e5a93cfd76de
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-09 15:46:37 +0530

    Create decorators.py

---
+++ Classrooms_Practice/9March/decorators.py
@@ -0,0 +1,27 @@
+class Student:
+    college='jecrc'
+    def __init__(self,name='Not Provided',
+    student_id='Not Provided',
+    branch='Not Provided'):
+        self.name=name
+        self.student_id=student_id
+        self.branch=branch
+    @classmethod
+    def chan(cls):
+        cls.college='poornima'
+    @staticmethod
+    def add():
+        print('huehue')
+        
+
+s3=Student('Akshay','it190','cs')
+print(s3.name,s3.student_id,s3.branch,s3.college)
+#call method 1
+s3.chan()
+#call method 2
+Student.chan()
+print(s3.name,s3.student_id,s3.branch,s3.college)
+s1=Student()
+print(s1.name,s1.student_id,s1.branch,s1.college)
+Student.add()
+s1.add()
