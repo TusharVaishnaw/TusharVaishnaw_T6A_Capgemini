9c905171b2f7d18247168a345572279278091739
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-09 10:57:58 +0530

    Create Task1_class.py

---
+++ Classrooms_Practice/9March/Task1_class.py
@@ -0,0 +1,15 @@
+class Student:
+    name='Not Provided'
+    student_id='Not Provided'
+    branch='Not Provided'
+s1=Student()
+s1.name='Tushar'
+s1.student_id='it176'
+s1.branch='it'
+s2=Student()
+s2.name='Akshat'
+s2.student_id='cs177'
+s2.branch='cs'
+
+print(s1.name,s1.student_id,s1.branch)
+print(s2.name,s2.student_id,s2.branch)
