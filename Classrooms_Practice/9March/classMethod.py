902bf2da4c5079079f6b64688667904e1bba6482
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-09 12:57:15 +0530

    Update classMethod.py

--- Classrooms_Practice/9March/classMethod.py
+++ Classrooms_Practice/9March/classMethod.py
@@ -16,7 +16,7 @@ print(s3.name,s3.student_id,s3.branch,s3.college)
 #call method 1
 s3.chan()
 #call method 2
-Student.chan()
+#Student.chan()
 print(s3.name,s3.student_id,s3.branch,s3.college)
 s1=Student()
 print(s1.name,s1.student_id,s1.branch,s1.college)
