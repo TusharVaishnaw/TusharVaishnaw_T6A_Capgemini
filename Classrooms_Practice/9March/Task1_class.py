86892d64531baae2549f6b3cc8007998e6f884db
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-09 11:01:37 +0530

    Update Task1_class.py

--- Classrooms_Practice/9March/Task1_class.py
+++ Classrooms_Practice/9March/Task1_class.py
@@ -2,11 +2,17 @@ class Student:
     name='Not Provided'
     student_id='Not Provided'
     branch='Not Provided'
+
+
 s1=Student()
+s2=Student()
+print(s1.name,s1.student_id,s1.branch)
+print(s2.name,s2.student_id,s2.branch)
+
 s1.name='Tushar'
 s1.student_id='it176'
 s1.branch='it'
-s2=Student()
+
 s2.name='Akshat'
 s2.student_id='cs177'
 s2.branch='cs'
