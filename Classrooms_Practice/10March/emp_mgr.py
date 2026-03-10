416835f52fe2c26ddc62b32b07fbd96c3d9c6429
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-10 12:45:18 +0530

    Create emp_mgr.py

---
+++ Classrooms_Practice/10March/emp_mgr.py
@@ -0,0 +1,16 @@
+class Employee:
+    
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+class Manager(Employee):
+
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary) 
+        self.department = department
+
+e1 = Manager("bxs", 11, "sk")
+print(e1.department)
+print(e1.name)
+print(e1.salary)
